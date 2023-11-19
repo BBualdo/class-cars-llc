42ac55a42486eb22a11510f3f7f7087c6442f64d
Sebastian <mrseb10@gmail.com>
2023-11-19 19:45:57 +0100

    Animated welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import { fadeIn } from "@/utils/fadeIn";
+import { motion } from "framer-motion";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between text-black bg-darkGrey">
-      <div className="absolute bg-five bg-no-repeat bg-center bg-cover w-full h-[600px] z-0 blur-sm" />
+    <main className="flex min-h-screen flex-col justify-between text-black bg-black">
+      <motion.div
+        variants={fadeIn("right", 0.3, 0.7)}
+        initial={"hidden"}
+        whileInView={"show"}
+        className="absolute bg-five bg-no-repeat bg-center bg-cover w-full h-[600px] z-0"
+      />
       <div className="w-full h-[600px] flex items-center justify-center">
-        <h1 className="text-[72px] text-transparent bg-clip-text bg-gradient-to-t from-yellow-600 to-yellow-100 font-extrabold z-10">
+        <motion.h1
+          variants={fadeIn("up", 0.8, 1)}
+          initial={"hidden"}
+          whileInView={"show"}
+          className={`text-transparent bg-clip-text bg-gradient-to-t from-yellow-600 to-yellow-100 font-extrabold z-10`}
+        >
           Witamy w ClassCarsLLC
-        </h1>
+        </motion.h1>
       </div>
     </main>
   );
--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,7 +1,27 @@
-import { Raleway, Merriweather } from "next/font/google";
+import {
+  Raleway,
+  Merriweather,
+  Dancing_Script,
+  Lobster_Two,
+  Neuton,
+  Ibarra_Real_Nova,
+} from "next/font/google";
 
 export const raleway = Raleway({ subsets: ["latin"] });
 export const merriweather = Merriweather({
   subsets: ["latin"],
   weight: ["300", "400", "700"],
 });
+export const dancing = Dancing_Script({
+  subsets: ["latin"],
+});
+export const lobster = Lobster_Two({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+export const neuton = Neuton({
+  subsets: ["latin"],
+  weight: ["400", "700", "800"],
+});
+
+export const ibarra = Ibarra_Real_Nova({ subsets: ["latin"] });
