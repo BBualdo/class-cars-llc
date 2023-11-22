c0521d049a1b77f825df9eb2e5fc6de4a48827f7
Sebastian <mrseb10@gmail.com>
2023-11-23 00:54:48 +0100

    Accessibility - Portfolio

--- src/components/portfolio/Slider.tsx
+++ src/components/portfolio/Slider.tsx
@@ -25,20 +25,24 @@ const Slider = ({
     <div className="flex w-full gap-10 xs:p-2 xs:max-md:flex-col md:p-8">
       <div className="flex h-auto w-[400px] flex-col gap-4">
         <div className="group relative flex h-[300px] w-full items-center overflow-hidden">
-          <ArrowBackIosNew
-            className="absolute left-0 z-40 -translate-x-full cursor-pointer bg-black/70 p-1 text-[40px] transition-all duration-300 group-hover:translate-x-0"
-            onClick={prevImage}
-          />
+          <button
+            aria-label="Previous Image"
+            className="absolute left-0 z-40 -translate-x-full bg-black/70 p-1 transition-all duration-300 focus:translate-x-0 group-hover:translate-x-0"
+          >
+            <ArrowBackIosNew className="text-[32px]" onClick={prevImage} />
+          </button>
           <Image
             src={images[current]}
             alt=""
             fill
             style={{ objectFit: "cover" }}
           />
-          <ArrowForwardIos
-            className="absolute right-0 z-40 translate-x-full cursor-pointer bg-black/70 p-1 text-[40px] transition-all duration-300 group-hover:translate-x-0"
-            onClick={nextImage}
-          />
+          <button
+            aria-label="Next Image"
+            className="absolute right-0 z-40 translate-x-full bg-black/70 p-1 transition-all duration-300 focus:translate-x-0 group-hover:translate-x-0"
+          >
+            <ArrowForwardIos className="text-[32px]" onClick={nextImage} />
+          </button>
         </div>
 
         <div className="flex w-full flex-wrap items-center justify-center gap-1">
