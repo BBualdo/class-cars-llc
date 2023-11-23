781854c43fb16f5e8f5ec5bac94367124a6c0ff5
Sebastian <mrseb10@gmail.com>
2023-11-23 12:40:10 +0100

    Accessibility for Portfolio

--- src/components/portfolio/Slider.tsx
+++ src/components/portfolio/Slider.tsx
@@ -23,16 +23,21 @@ const Slider = ({
 
   return (
     <div className="flex w-full gap-10 xs:p-2 xs:max-md:flex-col md:p-8">
-      <div className="flex h-auto w-[400px] flex-col gap-4">
+      <div
+        aria-label="Image Slider"
+        className="flex h-auto w-[400px] flex-col gap-4"
+      >
         <div className="group relative flex h-[300px] w-full items-center overflow-hidden">
           <button
             aria-label="Previous Image"
-            className="absolute left-0 z-40 -translate-x-full bg-black/70 p-1 transition-all duration-300 focus:translate-x-0 group-hover:translate-x-0"
+            className="absolute left-0 z-40 -translate-x-full bg-black/70 p-1 transition-all duration-300 focus-visible:translate-x-0 group-hover:translate-x-0"
           >
             <ArrowBackIosNew className="text-[32px]" onClick={prevImage} />
           </button>
+
           {images.map((image, index) => (
             <Image
+              aria-hidden={current !== index}
               key={index}
               src={image}
               alt=""
@@ -40,23 +45,24 @@ const Slider = ({
               height={300}
               style={{
                 translate: `${-100 * current}%`,
-                aspectRatio: 4 / 3,
+                aspectRatio: 3 / 2,
                 objectFit: "cover",
               }}
+              priority={current === index}
               className="transition-translate duration-300"
             />
           ))}
 
           <button
             aria-label="Next Image"
-            className="absolute right-0 z-40 translate-x-full bg-black/70 p-1 transition-all duration-300 focus:translate-x-0 group-hover:translate-x-0"
+            className="absolute right-0 z-40 translate-x-full bg-black/70 p-1 transition-all duration-300 group-hover:translate-x-0 group-focus-visible:translate-x-0"
           >
             <ArrowForwardIos className="text-[32px]" onClick={nextImage} />
           </button>
         </div>
 
         <div className="flex w-full flex-wrap items-center justify-center gap-1">
-          {images.map((image, index) => (
+          {images.map((_, index) => (
             <button
               key={index}
               aria-label={`Choose image button: ${index}`}
