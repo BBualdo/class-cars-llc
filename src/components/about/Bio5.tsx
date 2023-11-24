86f25dd2288dc66a5ba81ce141abb206e207dccf
Sebastian <mrseb10@gmail.com>
2023-11-24 21:03:21 +0100

    Bio 5 - Framer Motion

--- src/components/about/Bio5.tsx
+++ src/components/about/Bio5.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/fadeIn";
+
 const Bio5 = () => {
   return (
     <section className="flex flex-col gap-10 px-10">
-      <p className="text-center text-[26px] font-bold text-white">
+      <motion.p
+        variants={fadeIn("down", 0.7, 1, 1.2)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+        className="text-center text-[26px] font-bold text-white"
+      >
         Mój konik to samochody, takie jak: Nissan Patrol, Wrangler czy G-klasa,
         ze względu na ich zabezpieczenie przed korozją – to duży atut na rynku
         europejskim. Szczególnie cenię sobie samochody klasyczne, które są
@@ -9,18 +20,30 @@ const Bio5 = () => {
         uszkodzeń. W przyszłości chciałbym wprowadzić na rynek więcej
         nietypowych samochodów, takich jak: Bronco 1, Hummer H1, czy rzadkie
         modele Aston Martin.
-      </p>
+      </motion.p>
 
-      <div className="bg-offroad h-[400px] w-full bg-cover bg-fixed bg-center bg-no-repeat opacity-70 shadow-[0_0_40px] shadow-red-700" />
+      <motion.div
+        variants={fadeIn("", 0.7, 1, 1.2)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+        className="bg-offroad h-[400px] w-full bg-cover bg-fixed bg-center bg-no-repeat opacity-70 shadow-[0_0_40px] shadow-red-700"
+      />
 
-      <p className="text-center text-[26px] font-bold text-white">
+      <motion.p
+        variants={fadeIn("up", 0.7, 1, 1.2)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+        className="text-center text-[26px] font-bold text-white"
+      >
         W mojej opinii samochody terenowe to niezawodność i wytrzymałość. Widzę
         w nich nie tylko wartość użytkową, ale i inwestycyjną. Każdy samochód,
         który kupuję i sprzedaję, traktuję jak projekt – starannie analizuję
         jego stan, historię, by następnie dokonać niezbędnych napraw i
         modyfikacji. Fascynuje mnie przywracanie dawnej świetności tym
         klasycznym maszynom.
-      </p>
+      </motion.p>
     </section>
   );
 };
