9a673df577f266af14bb74c11c1b473e03249af9
Sebastian <mrseb10@gmail.com>
2023-11-26 15:39:09 +0100

    Contact Page Banner 1

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,9 @@
+import Banner1 from "@/components/contact/Banner1";
+
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
-      Contact Page
+    <main className="flex min-h-screen flex-col overflow-hidden">
+      <Banner1 />
     </main>
   );
 }
