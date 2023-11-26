994af9603833ea027f3ea1c17a69ed4475984f57
Sebastian <mrseb10@gmail.com>
2023-11-26 02:23:26 +0100

    Last Bio page in About - Framer Motion

--- src/components/about/Bio7.tsx
+++ src/components/about/Bio7.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/fadeIn";
+
 const Bio7 = () => {
   return (
     <section className="gradient-black_white flex w-full justify-center xs:max-md:pb-20 md:py-20">
-      <div className="flex flex-col items-center gap-10 border-4 border-gold bg-black p-4 text-center text-white shadow-[0_10px_50px] shadow-gold md:w-3/4">
+      <motion.div
+        variants={fadeIn("right", 0.7, 1, 1.2)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+        className="flex flex-col items-center gap-10 border-4 border-gold bg-black p-4 text-center text-white shadow-[0_10px_50px] shadow-gold md:w-3/4"
+      >
         <p className="font-bold md:text-[18px] lg:text-[24px]">
           Oprócz handlu samochodami, zajmuję się ich renowacją. Mam kilka
           projektów, gdzie odrestaurowuję klasyczne modele do ich oryginalnego
@@ -17,7 +28,7 @@ const Bio7 = () => {
           możliwości. Moim celem jest stać się uznawany nie tylko jako handlarz,
           ale jako ekspert i ambasador kultury motoryzacyjnej.
         </p>
-      </div>
+      </motion.div>
     </section>
   );
 };
