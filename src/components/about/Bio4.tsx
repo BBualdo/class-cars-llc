7911d744e7ce5aee6eafbffe252787f74002c347
Sebastian <mrseb10@gmail.com>
2023-12-06 22:15:14 +0100

    Replaced map with flag bg

--- src/components/about/Bio4.tsx
+++ src/components/about/Bio4.tsx
@@ -1,85 +1,77 @@
 "use client";
 
-import Image from "next/image";
-
 import { motion } from "framer-motion";
 import { fadeIn } from "@/utils/fadeIn";
 
 const Bio4 = () => {
   return (
     <section className="flex w-full py-20">
-      <motion.div
-        variants={fadeIn("right", 0.5, 1, 1.2)}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: true }}
-        className="relative h-[1000px] w-full"
-      >
-        <Image
-          alt=""
-          src="/template-images/map.avif"
-          fill
-          style={{ objectFit: "cover" }}
-          className="blur-[2px]"
-        />
-        <div className="absolute flex h-full w-full flex-col px-4 xs:justify-center xs:max-md:gap-2 md:justify-around">
+      <div className="w-full">
+        <div className="flex w-full flex-col px-4 xs:gap-2 md:gap-10">
           <motion.div
-            variants={fadeIn("left", 0.5, 1, 1.2)}
+            variants={fadeIn("", 0.5, 1, 1.2)}
             initial="hidden"
             whileInView="show"
             viewport={{ once: true }}
-            className="flex w-full"
+            className="flex w-full justify-center"
           >
-            <div className="bg-gray-700/70 p-4 md:w-3/4">
-              <p className="text-white/80 xs:text-[15px] md:text-[18px] lg:text-[24px]">
-                Importem samochodów zajmuję się od{" "}
-                <span className="text-gold">2017</span> roku. Wszystko zaczęło
-                się od importu aut z Niemiec, co dało mi solidne podstawy do
-                zrozumienia europejskiego rynku motoryzacyjnego. Wkrótce potem
-                rozszerzyłem działalność na import samochodów z Włoch i Francji,
-                co pozwoliło mi na poznanie różnorodności europejskiego rynku.
-              </p>
+            <div className="bg-europe bg-cover bg-center bg-no-repeat md:w-3/4 lg:w-1/2">
+              <div className="w-full p-8 backdrop-brightness-[0.2]">
+                <p className="text-center font-bold text-white md:text-[20px] lg:text-[26px]">
+                  Importem samochodów zajmuję się od{" "}
+                  <span className="text-gold">2017</span> roku. Wszystko zaczęło
+                  się od importu aut z Niemiec, co dało mi solidne podstawy do
+                  zrozumienia europejskiego rynku motoryzacyjnego. Wkrótce potem
+                  rozszerzyłem działalność na import samochodów z Włoch i
+                  Francji, co pozwoliło mi na poznanie różnorodności
+                  europejskiego rynku.
+                </p>
+              </div>
             </div>
           </motion.div>
           <motion.div
-            variants={fadeIn("right", 0.7, 1, 1.2)}
+            variants={fadeIn("", 0.5, 1, 1.2)}
             initial="hidden"
             whileInView="show"
             viewport={{ once: true }}
-            className="flex w-full justify-end"
+            className="flex w-full justify-center"
           >
-            <div className="bg-gray-700/70 p-4 md:w-3/4">
-              <p className="text-white/80 xs:text-[15px] md:text-[18px] lg:text-[24px]">
-                Z czasem, moje zainteresowanie przeniosło się na rynek
-                amerykański. Przeprowadzka do Kalifornii, w Stanach
-                Zjednoczonych, otworzyła przede mną nowe możliwości i pozwoliła
-                zgłębić tamtejszy rynek samochodowy. To doświadczenie było
-                nieocenione i znacznie wzbogaciło moją wiedzę oraz umiejętności
-                w dziedzinie handlu autami.
-              </p>
+            <div className="bg-usa bg-cover bg-center bg-no-repeat md:w-3/4 lg:w-1/2">
+              <div className="w-full p-8 backdrop-brightness-[0.2]">
+                <p className="text-center font-bold text-white md:text-[20px] lg:text-[26px]">
+                  Z czasem, moje zainteresowanie przeniosło się na rynek
+                  amerykański. Przeprowadzka do Kalifornii, w Stanach
+                  Zjednoczonych, otworzyła przede mną nowe możliwości i
+                  pozwoliła zgłębić tamtejszy rynek samochodowy. To
+                  doświadczenie było nieocenione i znacznie wzbogaciło moją
+                  wiedzę oraz umiejętności w dziedzinie handlu autami.
+                </p>
+              </div>
             </div>
           </motion.div>
           <motion.div
-            variants={fadeIn("left", 0.9, 1, 1.2)}
+            variants={fadeIn("", 0.5, 1, 1.2)}
             initial="hidden"
             whileInView="show"
             viewport={{ once: true }}
-            className="flex w-full"
+            className="flex w-full justify-center"
           >
-            <div className="bg-gray-700/70 p-4 md:w-3/4">
-              <p className="text-white/80 xs:text-[15px] md:text-[18px] lg:text-[24px]">
-                Następnie, podczas wizyty w Emiratach Arabskich, dostrzegłem
-                potencjał rynku motoryzacyjnego w Dubaju. Tutejszy rynek oferuje
-                atrakcyjne cenowo samochody, które stanowią ciekawą propozycję
-                dla kolekcjonerów oraz miłośników motoryzacji. Równolegle,
-                oprócz prężnej działalności w Emiratach Arabskich, rozpocząłem
-                import samochodów ze Szwecji, uzupełniając tym samym moje
-                doświadczenie o kolejny ważny kierunek europejski.
-              </p>
+            <div className="bg-uae bg-cover bg-center bg-no-repeat md:w-3/4 lg:w-1/2">
+              <div className="w-full p-8 backdrop-brightness-[0.2]">
+                <p className="text-center font-bold text-white md:text-[20px] lg:text-[26px]">
+                  Następnie, podczas wizyty w Emiratach Arabskich, dostrzegłem
+                  potencjał rynku motoryzacyjnego w Dubaju. Tutejszy rynek
+                  oferuje atrakcyjne cenowo samochody, które stanowią ciekawą
+                  propozycję dla kolekcjonerów oraz miłośników motoryzacji.
+                  Równolegle, oprócz prężnej działalności w Emiratach Arabskich,
+                  rozpocząłem import samochodów ze Szwecji, uzupełniając tym
+                  samym moje doświadczenie o kolejny ważny kierunek europejski.
+                </p>
+              </div>
             </div>
           </motion.div>
         </div>
-      </motion.div>
+      </div>
     </section>
   );
 };
