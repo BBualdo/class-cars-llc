c5f03ad25b225ab3af2910f1245b14e3767c2891
Sebastian <mrseb10@gmail.com>
2024-06-01 21:33:59 +0200

    Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import { raleway } from "@/fonts/fonts";
 import { StyledEngineProvider } from "@mui/material";
 import Footer from "@/components/footer/Footer";
 import ScrollToTop from "@/components/interface/ScrollToTop";
+import React from "react";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { config } from "@/lib/config";
 
 export default function RootLayout({
   children,
@@ -19,6 +22,7 @@ export default function RootLayout({
           <ScrollToTop />
           <Footer />
         </body>
+        <GoogleAnalytics gaId={config.gKey} />
       </html>
     </StyledEngineProvider>
   );
