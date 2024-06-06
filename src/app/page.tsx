0ebe3788d14c22770276d630f8927b1350ae4417
Flowie71 <153373830+Flowie71@users.noreply.github.com>
2024-06-06 21:50:45 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import Banner3 from "@/components/home/Banner3";
 import Banner4 from "@/components/home/Banner4";
 import Banner5 from "@/components/home/Banner5";
 import type { Metadata } from "next";
-import { GoogleTagManager } from '@next/third-parties/google'
+
+export const metadata: Metadata = {
+  title: "Auta z Dubaju - Import Samochodów | ClassCars",
+  description:
+    "ClassCars - Twój zaufany partner w imporcie aut z Dubaju do Polski. Oferujemy kompleksową obsługę, bezpieczny transport i atrakcyjne ceny. Sprawdź naszą ofertę!",
+  keywords: ["Auta z Dubaju"],
+};
 
 export default function Home() {
   return (
@@ -17,14 +23,3 @@ export default function Home() {
     </main>
   );
 }
-
-export function Page() {
-  return <GoogleTagManager gtmId="GTM-5KRGVNK6" />;
-}
-
-export const metadata: Metadata = {
-  title: "Auta z Dubaju - Import Samochodów | ClassCars",
-  description:
-    "Twój zaufany partner w imporcie aut z Dubaju do Polski. Oferujemy kompleksową obsługę, bezpieczny transport i atrakcyjne ceny. Sprawdź naszą ofertę!",
-  keywords: ["Auta z Dubaju"],
-};
