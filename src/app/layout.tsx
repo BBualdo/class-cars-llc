4f2c1cd61632479d3ef701eac4f1a278cc7ae424
Flowie71 <153373830+Flowie71@users.noreply.github.com>
2024-06-06 20:57:14 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,20 @@ import ScrollToTop from "@/components/interface/ScrollToTop";
 import React from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { config } from "@/lib/config";
+import { GoogleTagManager } from '@next/third-parties/google'
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <GoogleTagManager gtmId="GTM-XYZ" />
+      <body>{children}</body>
+    </html>
+  )
+}
 
 export default function RootLayout({
   children,
