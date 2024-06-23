c77a6d3b5abc123553f41886c34797eb4e5c82d6
Sebastian <mrseb10@gmail.com>
2024-06-23 20:52:05 +0200

    Articles update

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,12 @@
 import ArticleList from "@/components/blog/ArticleList";
 import Banner1 from "@/components/blog/Banner1";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog | ClassCarsLLC",
+  description:
+    "Interesują Cię samochody z Dubaju? Szukasz porad i ciekawostek dotyczących importu aut? Odwiedź blog ClassCars, aby znaleźć szczegółowe informacje o procesie importu aut oraz najlepszych praktykach zakupu aut z Dubaju.",
+};
 
 export default function Page() {
   return (
